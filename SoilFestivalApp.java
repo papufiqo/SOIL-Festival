976fca45b7596a2be549de8c6ac6e8748e1df4ed
papufiqo <donika.stavro@gmail.com>
2025-08-24 16:25:22 +0200

    Create SoilFestivalApp.java

---
+++ SoilFestivalApp.java
@@ -0,0 +1,11 @@
+package com.soil.festival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoilFestivalApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SoilFestivalApp.class, args);
+    }
+}
