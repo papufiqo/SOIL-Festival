7b4b78c56a7beacef73b8458709bb37852a2a06b
papufiqo <donika.stavro@gmail.com>
2025-08-24 16:25:58 +0200

    Create HealthController.java

---
+++ HealthController.java
@@ -0,0 +1,12 @@
+package com.soil.festival.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/api/v1/health")
+    public String health() {
+        return "OK - SOIL Festival Java Platform Running";
+    }
+}
